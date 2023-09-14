c0229933e12ec01eb6fd146026c437e139b5fa9c
sunjiajie03 <sunjiajie03@kuaishou.com>
2023-09-15 01:03:17 +0800

    feat: x64, arm64

--- src/index.ts
+++ src/index.ts
@@ -2,38 +2,40 @@ import * as path from 'path';
 import * as fs from 'fs-extra';
 import rebuild from '@electron/rebuild';
 
+const archs = ['x64', 'arm64'];
+
 const rebuildByElectronVersion = async (electronVersion: string) => {
-  const productName = `better_sqlite3_${electronVersion}.node`
+  for (const archName of archs) {
+    const productName = `better_sqlite3_${electronVersion}_${archName}.node`;
 
-  const destPath = path.resolve(__dirname, '../product', productName)
-  
-  if(fs.existsSync(destPath)){
-    console.log(`版本${electronVersion}已存在，跳过rebuild ======`);
-    return;
-  }
+    const destPath = path.resolve(__dirname, '../product', productName);
 
-  try {
-    console.log(`开始 rebuild ${electronVersion} ====`);
-    
-    await rebuild({
-      buildPath: path.resolve(__dirname, '../node_modules/better-sqlite3'),
-      electronVersion: electronVersion,
-      // arch: 'x86_64',
-      arch: 'x86_64'
-    });
+    if (fs.existsSync(destPath)) {
+      console.log(`版本${electronVersion}已存在，跳过rebuild ======`);
+      return;
+    }
 
-    console.log('rebuildByElectronVersion success !!!');
+    try {
+      console.log(`开始 rebuild ${productName} ====`);
 
-    // 复制到 product
-    fs.copySync(
-      path.resolve(__dirname, '../node_modules/better-sqlite3/build/Release/better_sqlite3.node'),
-      destPath,
-    );
+      await rebuild({
+        buildPath: path.resolve(__dirname, '../node_modules/better-sqlite3'),
+        electronVersion: electronVersion,
+        arch: archName,
+      });
 
-    console.log('文件复制成功');
-    
-  } catch (e) {
-    console.log(`rebuildByElectronVersion 失败 ！ ${e}`);
+      console.log('rebuildByElectronVersion success !!!');
+
+      // 复制到 product
+      fs.copySync(
+        path.resolve(__dirname, '../node_modules/better-sqlite3/build/Release/better_sqlite3.node'),
+        destPath,
+      );
+
+      console.log('文件复制成功');
+    } catch (e) {
+      console.log(`rebuildByElectronVersion 失败 ！ ${e}`);
+    }
   }
 };
 
